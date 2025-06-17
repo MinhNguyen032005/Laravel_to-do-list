5bae5c66556b2809c19bc087ad56e654c5b759d9
wanmin <23130193@st.hcmuaf.edu>
2025-06-17 15:13:45 +0700

    them data base vao va kt noi duoc data base

---
+++ main/laravel-app/database/migrations/2025_06_17_073402_create_task_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->boolean('is_completed')->default(false);
+            $table->timestamp('due_date')->nullable(); // hoặc Date nếu bạn thích
+            $table->timestamps(); // Tự thêm created_at và updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task');
+    }
+};
